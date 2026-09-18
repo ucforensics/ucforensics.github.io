6ea43d11ed5a36f2bbd6658f13dd04a225285ca5
ucforensics <ucccjeforensics@gmail.com>
2026-09-18 14:46:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ if (gallery) {
     const image = new Image(1200, 800);
     const number = String(index + 1).padStart(3, "0");
 
-    image.alt = `Conceptual forensic science illustration: ${subject}`;
+    image.alt = ``;
     image.decoding = "async";
     image.src = `i_forensic${number}.png`;
 
